3a2ed4fa4ec4d14898ef64f85a719fd03c6078b4
Jeanne Pelisson <jeanne.pelisson@essec.edu>
2019-06-13 23:23:45 +0200

    Revert "Revert "Revert "signupPt2"""
    This reverts commit 3c938642856fd910e4c46a5644578c8eb0354c3d.

--- index.js
+++ index.js
@@ -4,90 +4,26 @@ const multer  = require('multer');
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
-// Mongo const
 const uri = "mongodb+srv://lift:liftiscool@cluster0-4vzbn.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
+//lift / liftiscool
 
-// Espress + multer for file upload + handlebars const
 const app = express();
-const upload = multer({ dest: 'uploads/' }); 
+const upload = multer({ dest: 'uploads/' });
 app.engine('hbs', exphbs({defaultLayout: 'main.hbs'}));
 app.set('view engine', 'hbs');
 app.use(express.urlencoded());
 
 const port = 8080;
 
-// checks connection to Mongo
-MongoClient.connect(uri, function(err, client) {
-  if(err) {
-       console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-  }
-  console.log('Connected...');
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+app.get("/Assoces/SignUp/", (req, res)=>res.render("signup"));
 
-//credentials   lift / liftiscool
-
-
-// FEED
-app.get("/Feed/", (req, res)=>res.render("index"));
-app.use("/Feed/",express.static("Feed"));
-
-//                                     PT1 of Assoce profile
-
-// GET Signup page
-app.get("/Assoces/SignUp/", (req, res)=>res.render("signupPt1"));
-
-// POST new Assoce profile PT1
-app.post("/Add-Assoce/", (req, res) => {
-  let Assoce = req.body.Company;
-  let Email = req.body.Email;
-  let Password = req.body.Password;
-  let lastname = req.body.LastName;
-  let firstname = req.body.FirstName;
-
-  client.connect(err => {
-    const collection = client.db("Lift").collection("Assoces");
-    collection.insertOne({ Assoce_1: Assoce, Email_1: Email, Password_1: Password, First_Name_1: firstname, Last_Name_1:lastname }, function (err, obj) {
-      res.redirect("/Assoces/SignUp2/");
-    });
-  });
-});
-
-//                                     PT2 of Assoce profile
-
-// GET Signup page PT2
-app.get("/Assoces/SignUp2/", (req, res)=>res.render("signupPt2"));
-
-// POST new Assoce profile PT2
-app.post("/Post-Profile/", (req, res) => {
-  let Assoce = req.body.Company;
-  let Email = req.body.Email;
-  let Password = req.body.Password;
-  let lastname = req.body.LastName;
-  let firstname = req.body.FirstName;
-
-  upload.single('profilepic')
-
-  client.connect(err => {
-    const collection = client.db("Lift").collection("Assoces");
-    collection.insertOne({ Assoce_1: Assoce, Email_1: Email, Password_1: Password, First_Name_1: firstname, Last_Name_1:lastname }, function (err, obj) {
-      res.redirect("/Feed/");
-    });
-  });
-
-});
-
-
-// Pas besoin de liste des Assoce, à transformer pour avoir une liste des OFFRES
-app.get("/Offers/", (req, res) => {
+app.get("/Assoces/SignUp/", (req, res) => {
   let search = req.query.search;
   let update = req.query.update;
 
   client.connect(err => {
-    const collection = client.db("Lift").collection("Assoces");
+    const collection = client.db("school").collection("students");
     collection.find(search ? { name: { '$regex': search, '$options': 'i' } } : {}).toArray(function (err, result) {
       result = result.map(item => {
         if (item._id == update) {
@@ -97,11 +33,23 @@ app.get("/Offers/", (req, res) => {
         }
         return item;
       });
-      res.render("Assoces", { result, search });
+      res.render("students", { result, search });
     });
   });
 });
 
+
+app.post("/Assoces/SignUp/", (req, res) => {
+  let studentname = req.body.studentname;
+  let studentbid = req.body.studentbid;
+
+  client.connect(err => {
+    const collection = client.db("school").collection("students");
+    collection.insertOne({ name: studentname, BID: studentbid }, function (err, obj) {
+      res.redirect("/students/");
+    });
+  });
+});
 
 app.post("/AssoceDelete/:id", (req, res) => {
   let id = req.params.id;
