5c5ec14dde234a646dafd68771dbbfba516f3251
devdenberg <davdenberg@live.fr>
2019-06-17 10:46:40 +0200

    Addofferfixed

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const multer  = require('multer');
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
+const cookieParser = require('cookie-parser');
+const session = require('express-session')
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo')(session);
+
 // Mongo const
 const uri = "mongodb+srv://lift:liftiscool@cluster0-4vzbn.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
@@ -75,19 +80,76 @@ app.post("/Add-Assoce/", (req, res) => {
   let Password = req.body.Password;
   let lastname = req.body.LastName;
   let firstname = req.body.FirstName;
+  let profilepic = req.body.profilepic;
+  let nationality = req.body.nationality;
+  let language = req.body.language;
+  let activity = req.body.activity;
+  let size = req.body.size;
+  let location = req.body.location;
+  let causes = req.body.causes;
+  let about = req.body.about;
+  let website = req.body.website;
 
   client.connect(err => {
     const collection = client.db("Lift").collection("Assoces");
-    collection.insertOne({ Assoce_1: Assoce, Email_1: Email, Password_1: Password, First_Name_1: firstname, Last_Name_1:lastname }, function (err, obj) {
-      res.redirect("/Assoces/SignUp2/");
-    });
+    collection.insertOne(
+      { Assoce_1: Assoce,
+        Email_1: Email,
+        Password_1: Password,
+        First_Name_1: firstname,
+        Last_Name_1:lastname,
+        profilepic: profilepic , 
+        nationality: nationality, 
+        language: language,
+        activity: activity,
+        size: size,
+        location: location,
+        causes: causes,
+        about: about,
+        website: website 
+      },
+      function (err, obj) {
+      // rediriger vers publish an offer
+        res.redirect("/Feed/");
+      }
+    );
   });
 });
 
+//                                     NEW OFFER
+
+// GET Add-offer page
+app.get("/New-Offer/", (req, res)=>res.render("add_offer"));
+
+// Post new offer
+app.post("/Add-Offer/", (req, res) => {
+  let Assoce = req.body.Organization;
+  let Email = req.body.Email;
+  let profilepic = req.body.profilepic;
+  let description=req.body.Offer_Description
+
+  client.connect(err => {
+    const collection = client.db("Lift").collection("Offers");
+    collection.insertOne(
+      { Assoce_1: Assoce,
+        email_1: Email,
+        profilepic_1: profilepic , 
+        desc_1: description
+      },
+      function (err, obj) {
+      // rediriger vers publish an offer
+        res.redirect("/Feed/");
+      }
+    );
+  });
+});
+
+/*
 //                                     PT2 of Assoce profile
 
 // GET Signup page PT2
 app.get("/Assoces/SignUp2/", (req, res)=>res.render("signupPt2"));
+
 
 // POST new Assoce profile PT2
 app.post("/Post-Profile/", (req, res) => {
@@ -123,6 +185,24 @@ app.post("/Post-Profile/", (req, res) => {
   });
 
 });
+*/
+
+/*
+//                          USER SESSIONS          (What's the use ???)
+mongoose.connect('mongodb://localhost/my-database', {
+    useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+const db = mongoose.connection
+
+app.use(cookieParser());
+app.use(session({
+    secret: 'my-secret',
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: db })
+}));
+*/
 
 
 // Pas besoin de liste des Assoce, à transformer pour avoir une liste des OFFRES
